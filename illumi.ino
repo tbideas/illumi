3a66b056af623f883a7b010868e87159d0b6c0d7
Thomas Sarlandie <thomas@sarlandie.net>
2012-08-24 15:34:48 -0700

    UC: The lamp should get back to the last used color/light when you power it
     * Now saving last command in EEPROM after 5 seconds of inactivity (to avoid too much writes on EEPROM when user is changing colors rapidly).
     * Now expecting the Wifly module to have been pre-configured for the wireless network and with a speed of 115200. We will
     have to write some code to help the user do that.

--- illumi.ino
+++ illumi.ino
@@ -7,16 +7,18 @@
  *
  */
 
+#include <EEPROM.h>
 // Edit your wifi settings in settings.h so that it is easy to pull/push
 // on github without pushing those local settings.
 #include "settings.h"
-#include "Wifly.h"
-#include "IllumiWiflyDelegate.h"
 #include "ColorLED.h"
 
 // Define buffer sizes
-#define BUFFER_SIZE 100
-#define OUTPUT_SIZE 300
+#define BUFFER_SIZE 255
+
+// The last command will take up at most 254 bytes. 
+// The first byte is used to tell the length so the next available byte is @ 0x100
+#define EEPROM_LAST_CMD 0x0000 
 
 int red, green, blue;
 
@@ -26,25 +28,28 @@ ColorLED led(RED, GREEN, BLUE, COMMON_ANODE);
 ColorLED led(RED, GREEN, BLUE, COMMON_ANODE, LED_SHUTDOWN);
 #endif
 
-IllumiWiflyDelegate wDelegate(led);
-
 void setup() {
 
   int i;
 
   led.setColor(0, 0, 0);
   
+  Serial.setTimeout(1);
+  Serial.begin(115200);
+  
   Serial1.setTimeout(10);
-  Wifly wifly(Serial1, &wDelegate);
-  #ifdef WPA_PASSPHRASE
-  wifly.initializeWpa(SSID, WPA_PASSPHRASE);
-  #elif defined WEP_PASSWORD
-  wifly.initializeWep(SSID, WEP_PASSWORD);
-  #else
-  wifly.initialize(SSID);
-  #endif
-  delay(1000);
-  led.setColor(0,0,0);
+  Serial1.begin(115200);
+  
+  char *lastCommand = readLastCommand();
+  if (lastCommand != NULL) {
+    Serial.write("Reloading last command:");
+    Serial.write(lastCommand);
+    processCommand(lastCommand);
+  }
+  else {
+    Serial.write("Starting from scratch");
+    led.setColor(100, 0, 0);
+  }
   
   // Use D2 as an instruction counter to measure the speed of transmission
   pinMode(2, OUTPUT);
@@ -52,10 +57,17 @@ void setup() {
 
 byte counter = 0;
 
+char buffer[BUFFER_SIZE];
+unsigned long lastCommandTime = 0;
+char *lastCommandPtr;
+
 void loop() {
-  char buffer[BUFFER_SIZE];
-  char output[OUTPUT_SIZE];
-  
+  processWifiInterface();
+  processDebugInterface();  
+}
+
+void processWifiInterface()
+{
   if (Serial1.available()) {
     int len = Serial1.readBytes(buffer, BUFFER_SIZE);
     
@@ -69,34 +81,116 @@ void loop() {
     }
     buffer[len] = 0;
 
-    bool gotCommand = false;
-
-    if (strstr(buffer, "*OPEN*")) {
-      snprintf(output, OUTPUT_SIZE, "RGB%x%x%x\n", red, green, blue);
-      Serial1.write(output);
-      gotCommand = true;
-    }
-    
     char *rgbCommand = strstr(buffer, "RGB");
     if (rgbCommand != NULL) {
-      red = hexPairsToInt(rgbCommand + 3);
-      green = hexPairsToInt(rgbCommand + 5);
-      blue = hexPairsToInt(rgbCommand + 7);
-
-      led.setColor(red, green, blue);
-      
-      snprintf(output, OUTPUT_SIZE, "RGB%02x%02x%02x\n", red, green, blue);   
-      Serial1.write(output);
-      gotCommand = true;
+      processCommand(rgbCommand);
       
       digitalWrite(2, counter++ % 2 == 0 ? HIGH : LOW);
+
+      lastCommandTime = millis();
+      lastCommandPtr = rgbCommand;
     }
+    else {
+      Serial1.write("KO >");
+      Serial1.write((const uint8_t*)buffer, len);
+      Serial.write("KO >");
+      Serial.write((const uint8_t*)buffer, len);
+    }
+  }
+  // If we have not received a command in the last 5 seconds, save the last command in the EEPROM
+  if (lastCommandTime != 0 && millis() > lastCommandTime + 5000) {
+    Serial.write("Saving last command: ");
+    Serial.write(lastCommandPtr);
+    saveLastCommand(lastCommandPtr);    
+    lastCommandTime = 0;
+  }
+}
+
+void processDebugInterface()
+{
+  char *last;
+  
+  if (Serial.available()) {
+    char command = Serial.read();
+    switch (command) {
+      case '?':
+        Serial.write("\nUp and running!\n");
+        break;
+      case 'L':
+        Serial.write("\nLast Command (EEPROM):\n");
+        last = readLastCommand();
+        if (last != NULL)
+          Serial.write(last);
+        else
+          Serial.write("NULL");
+        break;
+      case 'l':
+        if (lastCommandTime != 0) {
+          Serial.write("\nLast Command Time = ");
+          Serial.write(lastCommandTime);
+          Serial.write(" And millis() is: ");
+          Serial.write(millis());
+          Serial.write("\nLast Command buffer points to:");
+          Serial.write(lastCommandPtr);
+        }
+        else {
+          Serial.write("No last command.\n");
+        }
+        break;
+      default:
+        Serial.write("Unknown command: ");
+        Serial.write(command);
+        Serial.write("\n");
+    }
+  }
+}
+
+void processCommand(char *command)
+{
+  // The only command format supported right now is RGBXXXXXX\n
+  red = hexPairsToInt(command + 3);
+  green = hexPairsToInt(command + 5);
+  blue = hexPairsToInt(command + 7);
+
+  led.setColor(red, green, blue);
+}
+
+void saveLastCommand(char *cmd)
+{
+  int i;
+  size_t len = strlen(cmd);
+  
+  int address = EEPROM_LAST_CMD;
+  if (len <= 0xFE) { // max len is 0xFF including 1 byte for length
+    EEPROM.write(address++, (byte)len);
     
-    if (!gotCommand)
+    Serial.write("Saving to EEPROM...\n");
+    for (i = 0; i < len; i++)
     {
-      snprintf(output, OUTPUT_SIZE, "KO >%s< (len=%i)\n", buffer, len);
-      Serial1.write(output);
+      Serial.write(*cmd);
+      EEPROM.write(address++, *cmd);
+      cmd++;
+    } 
+  }
+}
+
+char *readLastCommand()
+{
+  size_t len = EEPROM.read(EEPROM_LAST_CMD);
+
+  // If len is 0 there is nothing to read, and if len is 0xFF then the EEPROM
+  // has just never be written there.
+  if (len != 0 || len != 0xFF) {
+    int i;
+
+    for (i = 0; i < len; i++) {
+      buffer[i] = EEPROM.read(EEPROM_LAST_CMD + i + 1);
     }
+    buffer[i] = 0;
+    return buffer;
+  }
+  else {
+    return NULL;
   }
 }
 
